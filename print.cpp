040f7e7308425a666943bd75fc138818a2e4cfe4
Shima63 <szzzdhrd@memphis.edu>
2015-04-09 12:24:46 -0500

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -1,35 +1,16 @@
+/*
+ * This File Contains Some Additional Common Function
+ * By Amirhossein & Philip & Shima
+ * April 2015
+ * No Copyright
+ * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
+ */
+
 #include <iostream>
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <sstream>
 #include "print.h"
-
 using namespace std;
-
-// This "print_file" function Prints Messages on Files and Terminal. Message Is of String Type.
-
-void print_file ( string message, ofstream & ofs ) {
-    ofs << message << flush;
-    cout << message << flush;
-    return;
-}
-
-// This "print_file" function Prints numbers on Files and Terminal. Number Is of Int Type.
-
-void print_file ( int number, ofstream & ofs ) {
-    ofs << number << flush;
-    cout << number << flush;
-    return;
-}
-
-// This "string uppercase" function changes all the letters of the input string to upper case. ( For string )
-
-string uppercase ( string s ) {
-    string result = s;
-    for (int i=0; i < (int)s.size(); i++)
-        result[i] = toupper(s[i]);
-    return result;
-}    
-
 
