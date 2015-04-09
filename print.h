05a5b5d93078cef982c8086dcc70f804062b3a09
Shima63 <szzzdhrd@memphis.edu>
2015-04-09 12:23:49 -0500

    Update print.h

--- print.h
+++ print.h
@@ -1,3 +1,12 @@
+/*
+ * This File Contains Some Additional Common Function
+ * By Amirhossein & Philip & Shima
+ * April 2015
+ * No Copyright
+ * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
+ */
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
