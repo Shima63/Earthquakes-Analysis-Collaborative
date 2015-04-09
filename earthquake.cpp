257e7576ceeaf02128167eacd6a79aab3684cfa2
PhilipOgweno <logweno@memphis.edu>
2015-04-09 16:35:42 -0500

    Update earthquake.cpp
    Added get and set functions for magnitude and longitude

--- earthquake.cpp
+++ earthquake.cpp
@@ -5,3 +5,52 @@
  * No Copyright
  * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
  */
+void earthquake::set_lon(double a) {
+    try
+    {
+        if (a < -180 || a > +180)
+            throw (22);
+        else
+            lon = a;
+    }
+    catch (int e)
+    {
+        cout << "Value of lon is invalid" << e << endl;
+    }
+}
+void earthquake::set_lon(string a){
+    try
+    {
+        int b = str2int(a);
+        set_lon(b);
+    }
+    catch (int e)
+    {
+        set_lon(-1);
+    }
+}
+
+void earthquake::set_magnitude(double a) {
+    try
+    {
+        if (a < 0 || a > 12)
+            throw (22);
+        else
+            magnitude = a;
+    }
+    catch (int e)
+    {
+        cout << "Value of magnitude is invalid" << e << endl;
+    }
+}
+void earthquake::set_magnitude(string a){
+    try
+    {
+        int b = str2int(a);
+        set_magnitude(b);
+    }
+    catch (int e)
+    {
+        set_magnitude(-1);
+    }
+}
