da9885cc3d21c38985211577aea28d5373e39bfa
Shima63 <szzzdhrd@memphis.edu>
2015-04-09 13:05:53 -0500

    Update earthquake.h

--- earthquake.h
+++ earthquake.h
@@ -5,3 +5,24 @@
  * No Copyright
  * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
  */
+
+// Libraries
+
+#include <iostream>
+#include <string>
+#include "print.h"
+using namespace std;
+    
+class station {
+    
+    public:
+    
+        // Set and Get Functions
+
+        // Other Functions
+
+
+    private:
+    
+
+};
