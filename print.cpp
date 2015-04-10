09a222f0f4bc75b4feae4a2c723528839c1d0a0a
Shima63 <szzzdhrd@memphis.edu>
2015-04-09 20:35:58 -0500

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -21,3 +21,17 @@ void print_file ( string message, ofstream & ofs ) {
     cout << message << flush;
     return;
 }
+
+void toupper_str(std::string& str) {
+    
+    for (size_t i = 0; i < str.size(); ++i)
+        str[i] = toupper(str[i]);
+    
+}
+
+string toupper_str_C(std::string str) {
+    
+    toupper_str(str);
+    
+    return str;
+}
