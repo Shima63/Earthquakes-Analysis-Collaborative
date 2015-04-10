3b9491b92fa0f5597c370917206f8dc217234fc9
Shima63 <szzzdhrd@memphis.edu>
2015-04-09 20:38:44 -0500

    Update print.h

--- print.h
+++ print.h
@@ -13,6 +13,8 @@
 #include <sstream>
 using namespace std;
 
-    void print_file ( string, ofstream & );
-    void print_file ( int, ofstream & );
-    string uppercase ( string );
+void print_file ( string, ofstream & );
+void print_file ( int, ofstream & );
+string uppercase ( string );
+void toupper_str(std::string&);
+string toupper_str_C(std::string str);
