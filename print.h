a86e7174bb5c1a790cdde8fc29027d1196b10c49
amirhossein-naemi <amirhnaimi@gmail.com>
2015-04-14 01:34:17 -0500

    Fix ifndef

--- print.h
+++ print.h
@@ -6,6 +6,8 @@
  * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
  */
 
+#ifndef PRINT_H
+#define PRINT_H
 
 #include <iostream>
 #include <string>
@@ -18,3 +20,5 @@ void toupper_str(std::string&);
 string toupper_str_C(std::string str);
 void tolower_str(std::string&);
 string tolower_str_C(std::string str);
+
+#endif
