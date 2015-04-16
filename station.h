f6eb044807d18b27927dcfec28a5608d13925435
Shima63 <szzzdhrd@memphis.edu>
2015-04-16 14:22:41 -0500

    Update station.h
    Some indentation Fixing

--- station.h
+++ station.h
@@ -6,94 +6,71 @@
  * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
  */
 
+// External Libraries
+
 #ifndef STATION_H
 #define STATION_H
 
-// External Libraries
-
-#include "print.h"
-#include "earthquake.h"
-#include <iostream>
 #include <string>
+#include <string.h>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <sstream>
+#include <stdlib.h>
 using namespace std;
 
-
-
 // Defining Variables
-
 // Long_period (L), Short_period (B), Broadband (H)
 
-// Case Insensitive
-enum types_of_band {
+enum types_of_band { // Case Insensitive
     Long_period  = 1,
     Short_period = 2,
     Broadband    = 3
 };
 
-// Case Insensitive
-enum types_of_instrument {
+enum types_of_instrument { // Case Insensitive
     High_Gain,
     Low_Gain,
     Accelerometer
 };
 
-// Case Sensitive
-enum network_codes {
+enum network_codes { // Case Sensitive
     CE, CI, FA, N, NP, WR
 };
 
-// not needed
-enum Orientation {
-    N, E, Z
-};
-    
 class station {
     public:
     
         // Set and Get 
         
         // Done by Philip
-        string get_type_of_band_str();
-        types_of_band get_type_of_band() { return type_of_band; }
-        bool set_type_of_band(types_of_band);
-        bool set_type_of_band(string);
+        string get_type_of_band_str ();
+        types_of_band get_type_of_band () { return type_of_band; }
+        bool set_type_of_band ( types_of_band );
+        bool set_type_of_band ( string );
     
         // Done by Shima
-        string get_type_of_instrument_str();
-        types_of_instrument get_type_of_instrument() { return type_of_instrument; }
-        bool set_type_of_instrument(types_of_instrument);
-        bool set_type_of_instrument(string);
+        string get_type_of_instrument_str ();
+        types_of_instrument get_type_of_instrument () { return type_of_instrument; }
+        bool set_type_of_instrument ( types_of_instrument );
+        bool set_type_of_instrument ( string );
     
         // Done by Amir
-        string get_network_code_str();
-        network_codes get_network_code() { return network_code; }
-        bool set_network_code(network_codes);
-        bool set_network_code(string);
+        string get_network_code_str ();
+        network_codes get_network_code () { return network_code; }
+        bool set_network_code ( network_codes );
+        bool set_network_code ( string );
     
         // Done by Philip
-        string get_station_name() { return station_name; }
-        bool set_station_name(string);
+        string get_station_name () { return station_name; }
+        bool set_station_name ( string );
     
         // Done by Shima
-        string get_orientation() { return Orientation; }
-        bool set_orientation(string);
- 
-
-        // Other Functions
-        
-        // Done by Amir
-        int network_code_enum(string str_net_code);
-        int type_of_instrument_enum(string str_type_of_inst);
-        int type_of_band_enum(string str_type_of_band);
-        string type_of_inst_string(types_of_instrument type_of_inst);
-        
-        // Done by Shima
-        bool isok_Orientation(string);
-        bool isok_type_of_instrument(string);
-        bool isok_type_of_band(string);
-        bool isok_station_code(string);
-        bool isok_network_code(string);
-        bool is_there_any_err(station);
+        string get_orientation () { return Orientation; }
+        bool set_orientation ( string );
 
     private:
         network_codes       network_code;
@@ -101,8 +78,22 @@ class station {
         types_of_band       type_of_band;
         types_of_instrument type_of_instrument;
         string              Orientation;
-
 };
 
-// The end
+// Other Functions
+
+// Done by Amir
+int network_code_enum ( string str_net_code );
+int type_of_instrument_enum ( string str_type_of_inst );
+int type_of_band_enum ( string str_type_of_band );
+string type_of_inst_string ( types_of_instrument type_of_inst );
+        
+// Done by Shima
+bool isok_Orientation ( string );
+bool isok_type_of_instrument ( string );
+bool isok_type_of_band ( string );
+bool isok_station_code ( string );
+bool isok_network_code ( string );
+bool is_there_any_err ( station );
+
 #endif
