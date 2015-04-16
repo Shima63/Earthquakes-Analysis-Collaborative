b39f6618fd88ebabaaf665543098136081c27783
Shima63 <szzzdhrd@memphis.edu>
2015-04-16 12:18:59 -0500

    Update amir.philip.shima.cpp
    Some Indentation and Format Fixing

--- amir.philip.shima.cpp
+++ amir.philip.shima.cpp
@@ -7,12 +7,6 @@
  * Github account: https://github.com/Shima63/Earthquakes-Analysis-Collaborative.git
  */
 
-// Defined Libraries
-
-#include "earthquake.h"
-#include "station.h"
-#include "print.h"
-
 // External Libraries
 
 #include <stdlib.h>
@@ -25,6 +19,12 @@
 #include <ctype.h>
 #include <locale>
 using namespace std;
+
+// Defined Libraries
+
+#include "earthquake.h"
+#include "station.h"
+#include "print.h"
 
 // Global Variables
 
@@ -47,7 +47,7 @@ int process_file ( string );                                           // Done b
 // Main Program.
 // Return Zero on Success, Non-Zero in case of Failure.
 
-int main ( int argc, char * argv[] ) { // Able to Get Multiple Input Files
+int main ( int argc, char * argv[] ) { // To Be Able to Get Multiple Input Files
     
     // Local Variable Definition
     
@@ -56,7 +56,7 @@ int main ( int argc, char * argv[] ) { // Able to Get Multiple Input Files
 
     // Preparing the Log File Which Is for All the Input Files
 
-    open_output(log, "myprogram.log");
+    open_output ( log, "myprogram.log" );
     
     // For Loop Untill There Is No Other Input File.
 
@@ -75,7 +75,7 @@ int main ( int argc, char * argv[] ) { // Able to Get Multiple Input Files
         }    
     }
 
-    print(log, str);
+    print ( log, str );
 
     // Closing the Open Files
     
@@ -83,7 +83,6 @@ int main ( int argc, char * argv[] ) { // Able to Get Multiple Input Files
         log.close();
     }    
     cout << "Finished all!" << endl;
-    
     return 0;
 }
 
@@ -93,33 +92,31 @@ int main ( int argc, char * argv[] ) { // Able to Get Multiple Input Files
 
 // This open_input Function Is Openning the Input Files and Is Checking for Their Sanity
 
-int open_input(ifstream & ifile, string ifilename, ofstream & log) {
+int open_input ( ifstream & ifile, string ifilename, ofstream & log ) {
     
     // Sanity check on the file stream
     
-    ifile.open(ifilename.c_str());
-    if (!ifile.is_open()) {
+    ifile.open ( ifilename.c_str() );
+    if ( !ifile.is_open() ) {
         stringstream str;
         str << "Error! Cannot open input file: " << ifilename << endl;
-        print(log, str);
+        print ( log, str );
         return -1;
     }
-    
     return 0;
 }
 
-// This open_output Function Is Openning the OUTput and Log Files and Is Checking for Their Sanity
+// This open_output Function Is Openning the Output and Log Files and Is Checking for Their Sanity
 
-int open_output(ofstream & o, string ofilename) {
+int open_output ( ofstream & o, string ofilename ) {
     
     // Print on output file
     
-    o.open(ofilename.c_str());
-    if (!o.is_open()) {
+    o.open ( ofilename.c_str() );
+    if ( !o.is_open() ) {
         cout << "Error! The output file: " + ofilename + " is locked\n";
         return -1;
     }
-    
     return 0;
 }
 
@@ -156,20 +153,19 @@ int read_header ( ifstream & in, ofstream & log, earthquake & EQ ) {
     
     str << eID << endl;
     print ( log, str );
-    str.clear();
-    
+    str.clear ();
     EQ.earthquake_name = lnam;
     EQ.id = lID;
-    
     str << lnam << endl;
     print ( log, str, true );
-    
     return 0;
 }
 
 // This read_data Function Is Getting the Earthquake Stations' Data
 
-int read_data(ifstream & in, ofstream & out, ofstream & log, earthquake & EQ) {
+int read_data ( ifstream & in, ofstream & out, ofstream & log, earthquake & EQ ) {
+    
+    // Local Variables
     
     stringstream str;
     string eqnet, eqst, eqbnd, eqins, eqor;
@@ -177,123 +173,111 @@ int read_data(ifstream & in, ofstream & out, ofstream & log, earthquake & EQ) {
     int  cnt = 0, i = 0;
     bool noerr;
     
-    while (in >> eqnet) {
+    while ( in >> eqnet ) {
         cnt++;
         noerr = true;
-        try
-        {
+        try {
             in >> eqst >> eqbnd >> eqins >> eqor;
             
-            if (!eqtmp.set_network_code(eqnet))
-            {
+            if ( !eqtmp.set_network_code ( eqnet ) ) {
                 noerr = false;
                 str << "Entry # " << setw(3) << right << cnt
                 << " ignored. Invalid network.\n";
-                print(log, str);
+                print ( log, str );
             }
             
-            if (!eqtmp.set_station_name(eqst))
-            {
+            if ( !eqtmp.set_station_name ( eqst ) ) {
                 noerr = false;
                 str << "Entry # " << setw(3) << right << cnt
                 << " ignored. Invalid station name.\n";
-                print(log, str);
+                print ( log, str );
             }
             
-            if (!eqtmp.set_type_of_band(eqbnd))
-            {
+            if ( !eqtmp.set_type_of_band ( eqbnd ) ) {
                 noerr = false;
                 str << "Entry # " << setw(3) << cnt
                 << " ignored. Invalid band type.\n";
-                print(log, str);
+                print ( log, str );
             }
             
-            if (!eqtmp.set_type_of_instrument(eqins))
-            {
+            if ( !eqtmp.set_type_of_instrument ( eqins ) ) {
                 noerr = false;
                 str << "Entry # " << setw(3) << cnt
                 << " ignored. Invalid instrument.\n";
-                print(log, str);
+                print ( log, str );
             }
             
-            if (!eqtmp.set_orientation(eqor))
-            {
+            if ( !eqtmp.set_orientation ( eqor ) ) {
                 noerr = false;
                 str << "Entry # " << setw(3) << cnt
                 << " ignored. Invalid Orientation.\n";
-                print(log, str);
+                print ( log, str );
             }
             
-            if (is_there_any_err(eqtmp))
+            if ( is_there_any_err ( eqtmp ) ) {
                 throw (30);
+            }    
         }
-        catch (int e)
-        {
+        catch ( int e ) {
             // any other errors
             noerr = false;
             cout << "An exception occurred. Exception Nr. " << e << '\n';
         }
 
-        if (noerr) {
-            EQ.stations.push_back(eqtmp);
+        if ( noerr ) {
+            EQ.stations.push_back ( eqtmp );
             i++;
         }
     }
     
     EQ.invalid = cnt - i;
     EQ.valid = i;
-    if (EQ.valid < 1) {
+    if ( EQ.valid < 1 ) {
         str << "logor! input number should be equal or greater than 1\n";
-        print(log, str);
+        print ( log, str );
         return -1;
     }
-
     return 0;
 }
 
 // This process Function Is Doing the Processing
-int process(ifstream & ifile, ofstream & log, earthquake & EQ){
-    
-    double el = EQ.get_lon();
+
+int process ( ifstream & ifile, ofstream & log, earthquake & EQ ) {
     stringstream str, str2;
-    str.precision(3);
-    str << "# " << EQ.get_day() << " " << EQ.get_month_str() << " "
-    << EQ.get_year() << " " << setfill('0') << setw(2) << EQ.get_hour()
-    << ":" << setfill('0') << setw(2) << EQ.get_min() << ":" << setfill('0')
-    << setw(2) << EQ.get_sec() << '.' << setfill('0') << setw(3)
-    << std::setprecision(0) << std::fixed << EQ.get_ms() << " " << EQ.tz
-    << " " << EQ.get_magnitude_Type_str() << " " << std::fixed
-    << std::setprecision(1) << EQ.get_magnitude() << " "
-    << EQ.earthquake_name << " [" << EQ.id << "] (" << std::setprecision(2)
-    << std::fixed << EQ.get_lon() << ", " << std::fixed << EQ.get_lat()
-    << ", " << std::setprecision(1) << EQ.get_elv() << ")" << endl;
+    str.precision ( 3 );
+    str << "# " << EQ.get_day () << " " << EQ.get_month_str () << " "
+    << EQ.get_year () << " " << setfill ('0') << setw ( 2 ) << EQ.get_hour ()
+    << ":" << setfill ( '0' ) << setw ( 2 ) << EQ.get_min () << ":" << setfill ( '0' )
+    << setw ( 2 ) << EQ.get_sec () << '.' << setfill( '0' ) << setw ( 3 )
+    << setprecision ( 0 ) << fixed << EQ.get_ms () << " " << EQ.tz
+    << " " << EQ.get_magnitude_Type_str () << " " << fixed
+    << setprecision ( 1 ) << EQ.get_magnitude () << " "
+    << EQ.earthquake_name << " [" << EQ.id << "] (" << setprecision ( 2 )
+    << fixed << EQ.get_lon () << ", " << fixed << EQ.get_lat ()
+    << ", " << setprecision ( 1 ) << EQ.get_elv () << ")" << endl;
     
     EQ.sign = 0;
     
-    for (int i = 0; i < EQ.valid; i++) {
+    for ( int i = 0; i < EQ.valid; i++ ) {
         string s = EQ.stations[i].get_orientation();
         unsigned int n = s.size();
-        for (size_t j = 0; j < n; j++) {
-            
+        for ( size_t j = 0; j < n; j++ ) {
             str2 << EQ.id << '.'
-            << EQ.stations[i].get_network_code_str() << '.'
-            << EQ.stations[i].get_station_name() << '.'
-            << EQ.stations[i].get_type_of_band_str()
-            << EQ.stations[i].get_type_of_instrument_str()
+            << EQ.stations[i].get_network_code_str () << '.'
+            << EQ.stations[i].get_station_name () << '.'
+            << EQ.stations[i].get_type_of_band_str ()
+            << EQ.stations[i].get_type_of_instrument_str ()
             << s[j] << endl;
-            
             EQ.sign++;
         }
     }
     
     str << EQ.sign << endl;
-    
-    print(log, str, true);
-    
-    print(log, str2, true);
-
+    print ( log, str, true );
+    print ( log, str2, true );
     return 0;
 }
+
 // This Process_file Function Is Doing the Main Process of Producing Signals
 
 int process_file ( string fn ) {
@@ -309,8 +293,7 @@ int process_file ( string fn ) {
     
     err = open_input ( in, fn, log );
     if ( err ) {
-            str << "error in reading " << fn << " file. error code: " << err
-                << endl;
+            str << "error in reading " << fn << " file. error code: " << err << endl;
         return err;
     }
     
@@ -325,8 +308,7 @@ int process_file ( string fn ) {
     
     str << "Header read correctly!" << endl << endl;
     print ( log, str );
-    
-    
+
     err = open_output ( out, fn+".out" );
     if ( err ) {
         str << "error in open output file. error code: " << err << endl;
@@ -351,12 +333,11 @@ int process_file ( string fn ) {
     << endl << "Total valid entries read: "      << setw(7) << EQ.valid
     << endl << "Total signals produced: "        << setw(9) << EQ.sign << endl;
     
-    if ( in.is_open() ){
-        in.close();
+    if ( in.is_open () ) {
+        in.close ();
     }
-    if ( out.is_open() ) {
-        out.close();
+    if ( out.is_open () ) {
+        out.close ();
     }
-    
     return 0;
 }
