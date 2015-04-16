c8baba0b920237ba96deffaeb06e9870876b1d3e
amirhossein-naemi <amirhnaimi@gmail.com>
2015-04-16 18:12:20 -0500

    Added print.cpp to the make file

--- amir.philip.shima.mk
+++ amir.philip.shima.mk
@@ -12,8 +12,8 @@ OBJECTS = amir_philip_shima
 
 all: amir_philip_shima
 
-amir_philip_shima: amir.philip.shima.o earthquake.o station.o
-	${CC} -o amir_philip_shima amir.philip.shima.o earthquake.o station.o
+amir_philip_shima: amir.philip.shima.o earthquake.o station.o print.o
+	${CC} -o amir_philip_shima amir.philip.shima.o earthquake.o station.o print.o
 
 amir.philip.shima.o: amir.philip.shima.cpp
 	${CC} -c amir.philip.shima.cpp
@@ -24,6 +24,8 @@ earthquake.o: earthquake.cpp
 station.o: station.cpp
 	${CC} -c station.cpp 
 
+print.o: print.cpp
+	${CC} -c print.cpp
+    
 clean:
 	rm -rf ${OBJECTS}
-	
