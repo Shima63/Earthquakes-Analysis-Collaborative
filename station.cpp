d2268b327e8030883694e6d5adcaee8c1f890f19
amirhossein-naemi <amirhnaimi@gmail.com>
2015-04-16 11:51:48 -0500

    Update network_code(..) func

--- station.cpp
+++ station.cpp
@@ -77,6 +77,57 @@ bool station::set_station_name(string a) {
     }
 }
 
+bool isok_network_code(string str){
+
+    if (str.size() == 2)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            if (!isalpha(str[i]))
+                return false;
+            if (!isupper(str[i]))
+                return false;
+        }
+        // if return true, means everything is fine
+        return true;
+    }
+    return false;
+}
+
+string network_codes_str[5] = { "CE", "CI", "FA", "NP", "WR" };
+string station::get_network_code_str(){
+    return network_codes_str[network_code];
+}
+
+bool station::set_network_code(network_codes network_codes_value){
+    try
+    {
+        network_code = network_codes_value;
+        return true;
+    }
+    catch (int e)
+    {
+        //cout << "Value of station name is invalid" << e << endl;
+        return false;
+    }
+}
+
+bool station::set_network_code(string network_codes_value){
+    try
+    {
+        if (!isok_network_code(network_codes_value))
+            throw (51);
+        else
+            network_code = (network_codes)network_code_enum(network_codes_value);
+        return true;
+    }
+    catch (int e)
+    {
+        //cout << "Value of station name is invalid" << e << endl;
+        return false;
+    }
+}
+
 string types_of_band_strf[3] = { "Longperiod", "Shortperiod", "Broadband" };
 string types_of_band_str[3] = { "L", "B", "H" };
 char types_of_band_char[3] = { 'L', 'B', 'H' };
