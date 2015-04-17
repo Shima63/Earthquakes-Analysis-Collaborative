e4605740b299ba040d06386316a95f34d72bebb1
Shima63 <szzzdhrd@memphis.edu>
2015-04-16 21:43:38 -0500

    Update amir.philip.shima.mk

--- amir.philip.shima.mk
+++ amir.philip.shima.mk
@@ -9,7 +9,7 @@ CFLAGS =
 all: H8
 
 H8: amir.philip.shima.cpp
-	$(CC) $(CFLAGS) print.cpp earthquake.cpp station.cpp amir.philip.shima.cpp -o amir_philip_shima
+	$(CC) $(CFLAGS) print.cpp earthquake.cpp station.cpp amir.philip.shima.cpp -o amir.philip.shima
 
 clean:
 	rm -rf amir.philip.shima
